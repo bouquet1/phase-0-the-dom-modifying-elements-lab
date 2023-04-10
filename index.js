0f62b25f93996c301c3c7484c731b64829e09574
bouquet1 <bukuleta@gmail.com>
2023-04-09 21:22:40 -0700

    F-I-N-A-L-L-Y

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// document.getElementById('main').remove();
+// const newHeader = document.createElement("h1");
+// document.body.appendChild(newHeader);
+         // alternavive: newHeader.setAttribute('id', 'victory');
+// newHeader.id = "victory";
+// let name = "Buket";
+// newHeader.innerHTML =`${name} is the champion`;
+
+//after here
+const main = document.getElementById('main');
+  const newHeader = document.createElement('h1')
+  main.remove();
+  newHeader.id ="victory";
+  newHeader.textContent = "Baker is the champion"
+  document.body.append(newHeader);
